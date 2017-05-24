6538279a03acb3019e26a1de270a9b879d3ddb31
victorcarrero <victorcarrero24@gmail.com>
2017-05-23 20:45:00 -1200

    commit 3

--- ufps/index.php
+++ ufps/index.php
@@ -3,3 +3,5 @@ session_start();
 $_SESSION["usuario"] = array("usuario" => "usuario","estado"=>true,"mostrar"=>-1);
 header("Location: gui/panel/visualizar/visualizarRankingGeneral.php");
 System out 
+
+System2
